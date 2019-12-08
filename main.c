fb1ebe70831ed1a7a8d3065814434388c34dfab8
Samira Marie Sadat <samira.sadat48@gmail.com>
2019-12-08 13:32:38 -0600

    Adding comments of the program structure and who is assigned to each part

--- main.c
+++ main.c
@@ -1,1 +1,38 @@
 //Disk Scheduler
+//Team Members:
+//Samira Sadat
+//Brad Rasmussen
+//Jacob Cox
+
+//Structures and Variables
+//Disk Head
+//Randomized Queue - Array? Linked List? Global or passed into function?
+//Sorted Queue 
+//List of "Number of tracks traversed". Same size as above queues
+
+//Functions
+
+//main
+//initialize variables
+//call policy functions
+
+//Random Request Generator - Samira
+// -Fill the main queue with randomly generated numbers
+
+//Manual Initialization of Queue - Brad
+
+//FIFO - Samira
+
+//LIFO - Samira
+
+//Shortest Service Time First - Samira
+
+//SCAN - Brad
+
+//C-SCAN - Brad
+
+//N-STEP-Scan - Jake
+
+//FSCAN - Jake
+
+//Print Summary Table -Jake
